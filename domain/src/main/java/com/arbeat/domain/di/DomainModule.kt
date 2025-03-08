006409f2731496c1698094dafd33fb6f8077cdf1
nadim <achmadnadim@gmail.com>
2025-03-08 12:28:26 +0700

    feat: Initialize Shopper Project with Multi-Module Structure
    This commit initializes the Shopper project with a multi-module structure, including `presentation`, `data`, and `domain` modules. The following changes were made:
    
    -   **Project Structure:**
        -   Created `presentation`, `data`, and `domain` modules.
        -   Configured `settings.gradle.kts` to include these modules.
        -   Created basic module files, like build files, manifest, test files, etc.
        -   Created default resource files to support icon and theme in presentation module.
    -   **Gradle Configuration:**
        -   Added `gradle/libs.versions.toml` for dependency management.
        -   Updated `build.gradle.kts` and module-level build files to utilize the new `libs.versions.toml`.
        -   Configured `gradle.properties` for AndroidX, Kotlin code style, and non-transitive R class.
        -  Created `gradlew` and `gradlew.bat` for running gradle in unix and windows.
        -  Added `gradle-wrapper.properties` to define gradle distribution version.
    -   **.gitignore Files:**
        -   Added `.gitignore` files in the root, `.idea`, `data`, `domain`, and `presentation` directories.
        -   These files specify common files to ignore, such as IDE caches, build outputs, and local properties.
    -   **Android Manifests:**
        -   Added `AndroidManifest.xml` files for `data`, `domain`, and `presentation` modules.
        -   The `presentation` module's manifest includes the main activity definition.
    -   **Koin Modules:**
        -   Created empty Koin module files (`DataModule.kt`, `DomainModule.kt`, `PresentationModule.kt`) for future dependency injection setup.
    -   **UI Theme:**
        -   Defined base colors, themes, and typography in the `presentation` module.
        -   Created `MainActivity` with a basic Compose UI to display "Hello Android!".
    -   **Icon Assets:**
        -   Added necessary files and folders for app icons including vector graphics and webp format.
    - **Test:**
       - Added default test files for unit testing and instrumental test.
    - **Proguard:**
      - Added default proguard files for each module.
    
    This foundational setup provides a solid starting point for further development of the Shopper application.

---
+++ domain/src/main/java/com/arbeat/domain/di/DomainModule.kt
@@ -0,0 +1,7 @@
+package com.arbeat.domain.di
+
+import org.koin.dsl.module
+
+val domainModule = module {
+
+}
---
+++ presentation/src/main/java/com/arbeat/shopper/di/PresentationModule.kt
@@ -0,0 +1,7 @@
+package com.arbeat.shopper.di
+
+import org.koin.dsl.module
+
+val presentationModule = module {
+
+}
